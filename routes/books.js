7506952c7c6fe37f6d6eb79c5d4cf9e3afc95f27
Kawther Gacem <kaoutheralbertinagacem@gmail.com>
2023-05-31 18:57:15 +0100

    book creation

---
+++ routes/books.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const router = express.Router()
+const Book = require('../models/book')
+const Author = require('../models/author')
+const multer = require('multer')
+const upload = multer
+
+// All books route
+router.get('/', async (req,res)=> {
+    res.send('all books')
+})
+
+// New book route
+router.get('/new', async (req,res) => {
+    try {
+        const authors = await Author.find()
+        const book = new Book()
+        res.render('books/new',{
+            authors: authors,
+            book: book
+        }) 
+    } catch {
+        res.redirect('/books')
+    }
+})
+
+// Create book route
+router.post('/',async (req,res)=>{
+   const book = new Book({
+    title: req.body.title,
+    author: req.body.author,
+    publishDate: new Date(req.body.publishDate),
+    pageCount: req.body.pageCount,
+    description: req.body.description
+   })
+
+})
+module.exports = router
