884c539a225784552c9fe8a29d23f780bb3ed5da
Kawther Gacem <kaoutheralbertinagacem@gmail.com>
2023-06-06 12:09:07 +0100

    create/new/index for books

--- models/book.js
+++ models/book.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
 mongoose.set('debug', true);
 
+const path = require('path')
+const coverImageBasePath = 'uploads/bookCovers'
+
 const bookSchema = new mongoose.Schema({
     title:{
         type: String ,
@@ -10,17 +13,12 @@ const bookSchema = new mongoose.Schema({
         type: String
     },
     publishDate:{
-        type:Date,
+        type: Date,
         required: true
     },
     pageCount:{
         type:Number,
         required: true
-    },
-    createdAt:{
-        type: Date,
-        required:true,
-        default:Date.now
     },
     coverImageName:{
         type: String,
@@ -33,4 +31,11 @@ const bookSchema = new mongoose.Schema({
     }
 })
 
+bookSchema.virtual('coverImagePath').get(function(){
+    if (this.coverImageName!=null){
+        return path.join('/',coverImageBasePath,this.coverImageName )
+    }
+})
+
 module.exports = mongoose.model('Book',bookSchema)
+module.exports.coverImageBasePath=coverImageBasePath
