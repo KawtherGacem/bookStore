9bd5de9549e5228d0b518098f3bb03cce6f3689d
Kawther Gacem <kaoutheralbertinagacem@gmail.com>
2023-06-15 11:26:56 +0100

    authors completed

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
 const bookRouter = require('./routes/books')
 const bodyParser= require('body-parser')
-
+const methodOverride = require('method-override')
 dotenv.config()
 
 app.set("view engine", "ejs") //set ejs as our view engine 
@@ -18,7 +18,7 @@ app.set("layout", "layouts/layout")
 app.use(expressLayouts) 
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({limit : '10mb', extended: false }))
-
+app.use(methodOverride('_method'))
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL,{
             useNewUrlParser: true })
